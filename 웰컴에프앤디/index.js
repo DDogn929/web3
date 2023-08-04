b553372de50a4ee3f2f08aa7f7c59b44d0fc5c94
000 <130423139+DDogn929@users.noreply.github.com>
2023-08-04 12:55:55 +0900

    8/4

--- 웰컴에프앤디/index.js
+++ 웰컴에프앤디/index.js
@@ -2,9 +2,11 @@ const longAnime = document.querySelector('.main-anime');
 const plusAnime = document.querySelector('.main-anime-plus');
 const textAnime = document.querySelector('.main-anime-text');
 
-const textView = [Finance,Service,Data,People,Tech,Experience];
+const textView = ['Finance','Service','Data','People','Tech','Experience'];
 
 text = '';
+
+for(let i = 0, i )
 
 
 
